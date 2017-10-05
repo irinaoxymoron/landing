246486af1a76e95a4cb5bd8aaec22e01db40544d
Irina Andrianova <andrianova@ag-media.ru>
2017-10-05 11:35:37 +0300

    fix gulp-file

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,85 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const spritesmith = require('gulp.spritesmith');
-const rimraf = require('rimraf');
-const rename = require('gulp-rename');
+'use strict';
 
-/* -------- Server  -------- */
-gulp.task('server', function() {
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    newer = require('gulp-newer'),
+    csso = require('gulp-csso'), // минификатор CSS
+    postcss = require('gulp-postcss'), // npm install --save-dev gulp-postcss
+    autoprefixer = require('autoprefixer'), // npm install autoprefixer --save-dev
+    watch = require('gulp-watch'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    browserify = require('gulp-browserify'),
+    babel = require('gulp-babel'),
+    fileinclude = require('gulp-file-include'),
+    browserSync = require('browser-sync').create(),
+    argv = require('yargs').argv,
+    prod = argv.prod; // минификация
+
+
+var app_dir = 'app';
+var wp_dir = 'dist';
+
+
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
-            port: 9000,
-            baseDir: "build"
+            baseDir: wp_dir
         }
     });
-
-    gulp.watch('build/**/*').on('change', browserSync.reload);
 });
 
-/* ------------ Pug compile ------------- */
-gulp.task('templates:compile', function buildHTML() {
-    return gulp.src('source/template/index.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('build'))
+gulp.task('sass', function () {
+    return gulp.src(app_dir + '/scss/**/*.scss')
+        .pipe(gulpif(!Boolean(prod), sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer({browsers: ['last 5 versions']})]))
+        .pipe(gulpif(Boolean(prod), csso()))
+        .pipe(gulpif(!Boolean(prod), sourcemaps.write()))
+        .pipe(gulp.dest(wp_dir + '/css'));
 });
 
-/* ------------ Styles compile ------------- */
-gulp.task('styles:compile', function () {
-    return gulp.src('source/styles/main.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename('main.min.css'))
-        .pipe(gulp.dest('build/css'));
+gulp.task('moveJs', function () {
+    return gulp.src(app_dir + '/js/*.js')
+        .pipe(gulpif(Boolean(prod), babel({
+            presets: ['es2015']
+        })))
+        .pipe(gulpif(Boolean(prod), uglify()))
+        .pipe(gulp.dest(wp_dir + '/js'));
+});
+/*   pipe(pagebuilder(app_dir))         */
+gulp.task('moveHtml', function () {
+    return gulp.src(app_dir + '/*.html')
+        .pipe(fileinclude())
+        .pipe(gulp.dest(wp_dir));
 });
 
-/* ------------ Sprite ------------- */
-gulp.task('sprite', function(cb) {
-    const spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
-        imgName: 'sprite.png',
-        imgPath: '../images/sprite.png',
-        cssName: 'sprite.scss'
-    }));
-
-    spriteData.img.pipe(gulp.dest('build/images/'));
-    spriteData.css.pipe(gulp.dest('source/styles/global/'));
-    cb();
+gulp.task('moveImg', function () {
+    return gulp.src(app_dir + '/img/**.*')
+        .pipe(gulp.dest(wp_dir + '/img'));
 });
 
-/* ------------ Delete ------------- */
-gulp.task('clean', function del(cb) {
-    return rimraf('build', cb);
+gulp.task('moveFonts', function () {
+    return gulp.src(app_dir + '/fonts/**.*')
+        .pipe(gulp.dest(wp_dir + '/fonts'));
+})
+
+
+gulp.task('watch', function () {
+    gulp.watch(app_dir + '/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch(app_dir + '/js/*.js', gulp.series('moveJs'));
+    gulp.watch(app_dir + '/**/*.html', gulp.series('moveHtml'));
+    gulp.watch(app_dir + '/img/*.*', gulp.series('moveImg'));
+    // gulp.watch(app_dir + '/**/*').on('change', browserSync.reload);
 });
 
-/* ------------ Copy fonts ------------- */
-gulp.task('copy:fonts', function() {
-    return gulp.src('./source/fonts/**/*.*')
-        .pipe(gulp.dest('build/fonts'));
-});
+gulp.task('rebase', gulp.series(
+    'moveHtml',
+    'moveImg',
+    'moveJs',
+    'sass',
+    'moveFonts'
+))
 
-/* ------------ Copy images ------------- */
-gulp.task('copy:images', function() {
-    return gulp.src('./source/images/**/*.*')
-        .pipe(gulp.dest('build/images'));
-});
-
-/* ------------ Copy ------------- */
-gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
-
-/* ------------ Watchers ------------- */
-gulp.task('watch', function() {
-    gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
-    gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
-});
-
-gulp.task('default', gulp.series(
-    'clean',
-    gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
-    gulp.parallel('watch', 'server')
-    )
-);
+gulp.task('default', gulp.series('rebase', gulp.parallel('browser-sync', 'watch')));
