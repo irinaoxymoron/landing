2d10f206f2fdfc648c86e3131ee920d0e0ac9203
Irina Andrianova <andrianova@ag-media.ru>
2017-10-11 13:45:43 +0300

    Open and close functionality were added to form

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -23,12 +23,13 @@
 
     window.form = me;
 
-    var openFormButton = document.querySelector('.arrow-down');
-
-    if (openFormButton) {
-        openFormButton.addEventListener('click', function (e) {
-            e.preventDefault();
-            form.open();
-        })
-    }
 }());
+
+var openFormButton = document.querySelector('.arrow-down');
+
+if (openFormButton) {
+    openFormButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        form.open();
+    })
+}
