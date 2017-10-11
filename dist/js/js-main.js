e48b9058923d547a8d33a6ca2e3e77356984e9da
Irina Andrianova <andrianova@ag-media.ru>
2017-10-11 15:42:20 +0300

    Simple validation was added

--- dist/js/js-main.js
+++ dist/js/js-main.js
@@ -1,4 +1,25 @@
 // form
+window.ITVDN = {};
+
+var validation = (function () {
+    var me = {};
+
+    me.isEmail = function(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    };
+
+    me.isNumber = function(number) {
+        var re = /^\d+$/;
+        return re.test(number);
+    };
+
+    me.isNotEmpty = function(str) {
+        return Boolean(str);
+    };
+
+    ITVDN.validation = me;
+}());
 
 (function () {
     var me = {};
@@ -21,15 +42,69 @@
         form.classList.add('is-hidden');
     };
 
-    window.form = me;
+    me.isValid = function () {
+        var requiredFields = document.querySelectorAll('[data-valid="required"]');
+        var emailValue = document.querySelector('[data-email]').value;
+        var numberValue = document.querySelector('[data-number]').value;
+
+        if (!me.isAllCompleted(requiredFields)) {
+            console.log('Заполните, пожалуйста, все необходимые поля');
+            return false;
+        } else if (!ITVDN.validation.isEmail(emailValue)) {
+            console.log('Некорректный email');
+            return false;
+        } else if (!ITVDN.validation.isNumber(numberValue)) {
+            console.log('Некорректный номер');
+            return false;
+        }
+
+        return true;
+    };
+
+    me.isAllCompleted = function (data) {
+        var result = true;
+
+        for (var i = 0; i < data.length; i++) {
+            if (!ITVDN.validation.isNotEmpty(data[i].value)) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    };
+
+    ITVDN.form = me;
 
 }());
 
-var openFormButton = document.querySelector('.arrow-down');
+(function () {
+    // Add event listeners
+    var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
 
-if (openFormButton) {
-    openFormButton.addEventListener('click', function (e) {
-        e.preventDefault();
-        form.open();
-    })
-}
+    if (openFormButton) {
+        openFormButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            ITVDN.form.open();
+        })
+    }
+
+    if (form) {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            if (ITVDN.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+        })
+    }
+}());
+
+
+
+
+
+
+
